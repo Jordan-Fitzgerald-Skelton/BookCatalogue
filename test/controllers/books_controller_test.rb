d8b28fc63231b018149df4e4a4c1dbb03274aede
x21366 <jordanfitzskelton45@gmail.com>
2024-10-24 11:30:06 +0100

    Update books_controller_test.rb

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -35,4 +35,39 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :no_content
   end
+
+  test "should not update book no title" do
+    patch book_url(@book), params: { book: { author: @book.author, description: @book.description, genre: @book.genre, pages: @book.pages, price: @book.price, rating: @book.rating, title: "" } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no author" do
+    patch book_url(@book), params: { book: { author: "", description: @book.description, genre: @book.genre, pages: @book.pages, price: @book.price, rating: @book.rating, title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no description" do
+    patch book_url(@book), params: { book: { author: @book.author, description: "", genre: @book.genre, pages: @book.pages, price: @book.price, rating: @book.rating, title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no genre" do
+    patch book_url(@book), params: { book: { author: @book.author, description: @book.description, genre: "", pages: @book.pages, price: @book.price, rating: @book.rating, title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no pages" do
+    patch book_url(@book), params: { book: { author: @book.author, description: @book.description, genre: @book.genre, pages: "", price: @book.price, rating: @book.rating, title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no price" do
+    patch book_url(@book), params: { book: { author: @book.author, description: @book.description, genre: @book.genre, pages: @book.pages, price: "", rating: @book.rating, title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
+
+  test "should not update book no rating" do
+    patch book_url(@book), params: { book: { author: @book.author, description: @book.description, genre: @book.genre, pages: @book.pages, price: @book.price, rating: "", title: @book.title } }, as: :json
+    assert_not (200...299).include?(response.code.to_i)
+  end
 end
