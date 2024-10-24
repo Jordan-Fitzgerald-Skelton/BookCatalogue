5a1737cb949c82270b413ec014d05b42fd038e97
x21366 <jordanfitzskelton45@gmail.com>
2024-10-24 12:10:33 +0100

    Create book_decorator.rb
    (might be removed in the future)

---
+++ app/decorators/book_decorator.rb
@@ -0,0 +1,34 @@
+require "delegate"
+# Decorator design pattern
+# Sets the format for each parameter
+class BookDecorator < SimpleDelegator
+  def formatted_title
+    "Title: #{title}"
+  end
+
+  def formatted_author
+    "Author: #{author}"
+  end
+
+  def formatted_description
+    "Description: #{description}"
+  end
+
+  def formatted_genre
+    "Genre: #{genre}"
+  end
+
+  def formatted_pages
+    "Pages: #{pages}"
+  end
+
+  def formatted_price
+    "Pages: #{price}"
+  end
+
+  def formatted_rating
+    "Rating: #{rating} / 5"
+  end
+
+
+end
