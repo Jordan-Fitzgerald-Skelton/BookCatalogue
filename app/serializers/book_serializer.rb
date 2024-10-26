21598aeaada2e594e20c8456ac29a2e12e31c94c
Jordan Fitzgerald Skelton <jordanfitzskelton45@gmail.com>
2024-10-26 12:17:30 +0100

    Update book_serializer.rb

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,7 +1,7 @@
 # Specifies which attributes of the Book model will be included in the JSON response
 # Also includes a decorator design pattern for formatted attributes
 class BookSerializer < ActiveModel::Serializer
-  attributes :formatted_title, :formatted_author, :formatted_description, :formatted_genre, :formatted_pages, :formatted_price, :formatted_rating
+  attributes :id, :formatted_title, :formatted_author, :formatted_description, :formatted_genre, :formatted_pages, :formatted_price, :formatted_rating
 
   def formatted_title
     BookDecorator.new(object).formatted_title
