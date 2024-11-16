26d141553c46981d8e5e2bae0fe33f252bbd3701
Your Name <jordanfitzskelton45@gmail.com>
2024-11-16 00:40:54 +0000

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,5 @@ class Book < ApplicationRecord
     validates :genre, presence: true
     validates :pages, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
-    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :pages, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
